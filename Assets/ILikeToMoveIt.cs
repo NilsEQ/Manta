4d9f5131c396669d7559bee7033981615eea5fa3
Nils QUAETAERT <nils.quaetaert@intra.gobelins.fr>
2022-10-12 15:32:48 +0200

    Beginning the ray

--- Assets/ILikeToMoveIt.cs
+++ Assets/ILikeToMoveIt.cs
@@ -5,10 +5,23 @@ using UnityEngine;
 
 public class ILikeToMoveIt : MonoBehaviour
 {
-    public float speed = 0.01f;
+    public float minspeed = 0.05f;
+    public float maxspeed = 0.5f;
+    public float currentSpeed = 0.5f;
+
     public float maxheight;
     public float minheight;
     public float goodheight;
+
+    public float maxXhigh = 3.1f;
+    public float maxXlow = 1.2f;
+
+
+    public float maxZlow = 0.22f;
+    public float maxZhigh= 0.4f;
+
+
+    public float rotateSpeed = 0.1f;
     // Start is called before the first frame update
     void Start()
     {
@@ -18,7 +31,7 @@ public class ILikeToMoveIt : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.Rotate(0, 0.01f, 0);
-        transform.position += speed * 0.01f * transform.forward;
+        transform.Rotate(0, rotateSpeed, 0);
+        transform.position += currentSpeed * 0.01f * transform.forward;
     }
 }
